eb198793122d5aeeb4103449519995df59c3cec5
ProviceUnify <ilyapravotkin@gmail.com>
2023-04-14 23:49:37 +0300

    Эксперименты с json и структурой хранения подключенных компонентов

--- SSWControl/Program.cs
+++ SSWControl/Program.cs
@@ -1,8 +1,24 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using SSWControl.Data;
 
+public class HCUI_Entries 
+{
+    public string Name { get; set; }
+    public string Uri { get; set; }
+}
 
+public class HCUI
+{
+    public List<HCUI_Entries> HealthChecks = new List<HCUI_Entries>();
+    public string? PollingRate = "10";
+
+}
+public class HC_Settings
+{
+    public HCUI? HealthChecksUI;
+}
 internal class Program
 {
     private static void Main(string[] args)
@@ -10,14 +26,16 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
         // Add services to the container.
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Configuration.AddJsonFile("healthcheck-settings.json"); // настраиваемая конфигурация подключенных компонентов
+        int HC_pollingRate = builder.Configuration.GetSection("HealthChecksUI").GetValue<int>("PollingRate");
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(connectionString));
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
+        builder.Services.AddControllers().AddNewtonsoftJson();
         builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             .AddEntityFrameworkStores<ApplicationDbContext>(); //авторизация
         builder.Services.AddRazorPages();
-        builder.Services.AddHealthChecksUI().AddInMemoryStorage(); // UI чекера здоровья
+        builder.Services.AddHealthChecksUI(settings => { settings.SetEvaluationTimeInSeconds(HC_pollingRate); }).AddInMemoryStorage(); // UI чекера здоровья
 
         var app = builder.Build();
 
@@ -45,7 +63,25 @@ internal class Program
         app.UseAuthentication();
         app.UseAuthorization();
         app.MapRazorPages();
-        app.Run();
 
+        // эксперименты с записью в json
+        StreamReader r = new StreamReader("healthcheck-settings.json");
+        var json = r.ReadToEnd();
+        r.Close();
+        HC_Settings ser = JsonConvert.DeserializeObject<HC_Settings>(json);
+        ser.HealthChecksUI.HealthChecks.Add(new HCUI_Entries { Name = "ttt", Uri = "uuu"});
+        ser.HealthChecksUI.HealthChecks.Add(new HCUI_Entries { Name = "zzz", Uri = "yyy"});
+        ser.HealthChecksUI.PollingRate = "43";
+
+        json = JsonConvert.SerializeObject(ser);
+        JsonSerializer serializer = new JsonSerializer();
+        serializer.Formatting = Formatting.Indented;
+        using (StreamWriter sw = new StreamWriter("healthcheck-settings.json"))
+        using (JsonWriter writer = new JsonTextWriter(sw))
+        {
+            serializer.Serialize(writer, ser);
+        }
+
+        app.Run();
     }
 }
